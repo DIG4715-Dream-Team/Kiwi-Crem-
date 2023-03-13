ac0e3a01f14d3534f410e6e0c4adc85eb718a484
PyroAce64 <BlazingAce64@gmail.com>
2023-03-12 23:10:15 -0400

    Movement done David's Workshop
    Finished movement script, player turns smoothly. Had a few problems and kind of added my own type of camera scripts. Also I downloaded a cinemachine camera to make it work.

--- Kiwi Cremé/Assets/Scripts/PlayerController.cs
+++ Kiwi Cremé/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public CharacterController controller;
+    public Transform cam;
 
-    Rigidbody rb;
+    public float speed = 6f;
 
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal"); 
-        float zMove = Input.GetAxisRaw("Vertical");
-   
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
